1b9a20d486529d3f3c3a444f521ba48069325414
Genevieve Evans <115845477+glevans@users.noreply.github.com>
2026-01-20 00:46:50 +0000

    Update setup.py
    Updated version 1.2.2.3 in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="mmcif_gen",
-    version="1.2.2.2",
+    version="1.2.2.3",
     packages=find_packages(),
     install_requires=[
         "argparse",
